0114ad3c59ff0ad3b3044a8b67350bc008e49333
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-09 11:02:13 -0400

    Complete assignment some help from official solutions

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+# Some parts written with the assistance of the official solutions
+
+class School
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    Hash[ @roster.map{|ky,vu| [ky, vu.sort] } ]
+  end
+end
